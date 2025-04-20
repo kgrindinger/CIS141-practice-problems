8a6ca4ddc698fcb2ca1d608f19a99e616a9a4f89
Kerry G <kerrylyn@gmail.com>
2025-04-20 01:13:35 -0700

    problem2.py

--- MOD2/problem2.py
+++ MOD2/problem2.py
@@ -1,1 +1,7 @@
-
+'''Create a program that prompts for 2 numbers and then outputs the addition, subtraction,
+multiplication, and division of the first number by the second number.
+'''
+print(3+7)
+print (10-8)
+print (9*9)
+print (25//5)
