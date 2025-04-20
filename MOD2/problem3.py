17cd5f4d7a637cb721c7866ed7c1d33be936cf55
Kerry G <kerrylyn@gmail.com>
2025-04-20 01:14:00 -0700

    problem3.py

--- MOD2/problem3.py
+++ MOD2/problem3.py
@@ -1,1 +1,10 @@
+'''
+Create a program that prompts for the side lengths of a triangle and computes the area using Heron's formula.
+(https://en.wikipedia.org/wiki/Heron%27s_formula)
+'''
+import math
+a = float(input('15'))
+b = float(input('9'))
+c = float(input('18'))
+print("a + b +c")
 
