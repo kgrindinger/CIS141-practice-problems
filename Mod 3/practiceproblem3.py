2f70ad912fed9ec2ac2b8997b3a91fd87db6f4d7
Kerry G <kerrylyn@gmail.com>
2025-04-25 00:39:30 -0700

    Update practiceproblem3.py

--- Mod 3/practiceproblem3.py
+++ Mod 3/practiceproblem3.py
@@ -1,1 +1,8 @@
+'''
+#Prompt the user for a sentence and a word to try to find in that sentence. Have the program 
+print out whether the word was found in the sentence. (i.e. True or False)
+'''
 
+sentence = input ("Please write a sentence.")
+
+print ("cloud" in sentence)
