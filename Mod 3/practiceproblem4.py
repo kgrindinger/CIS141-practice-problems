5a6e9bb06cfcffcf8fcd4f5766096a87dafd6b3c
Kerry G <kerrylyn@gmail.com>
2025-04-25 00:47:45 -0700

    Update practiceproblem4.py

--- Mod 3/practiceproblem4.py
+++ Mod 3/practiceproblem4.py
@@ -1,1 +1,10 @@
+'''
+#Prompt the user for: a word, a first index, and a last index. 
+Slice the word at the indexes provided by the user and print to the screen.
+'''
 
+str = (input ("Please type a word."))
+
+index1 = (input ("Please pick a first index "))
+index2 = (input ("Please pick a last index "))
+print (str [0:5])
