1e155950d254c75f085ed87f24c8178642850847
Kerry G <kerrylyn@gmail.com>
2025-04-25 00:59:18 -0700

    Update practiceproblem5.py

--- Mod 3/practiceproblem5.py
+++ Mod 3/practiceproblem5.py
@@ -1,1 +1,12 @@
+'''
+#Print 3 words with a | character (called a pipe) in between them. 
+Use the appropriate keyword argument in print() to do so.# Write your code here :-)
+'''
 
+phrase = "rock and roll"
+
+print(phrase)
+
+new_phrase = "|".join (phrase.split())
+
+print(new_phrase)
