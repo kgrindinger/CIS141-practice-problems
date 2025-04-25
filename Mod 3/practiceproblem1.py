5556d2035ccf409380797e1dfb6a47288cb84149
Kerry G <kerrylyn@gmail.com>
2025-04-25 00:35:58 -0700

    Update practiceproblem1.py

--- Mod 3/practiceproblem1.py
+++ Mod 3/practiceproblem1.py
@@ -1,1 +1,8 @@
+# Prompt the user for a word. Then, prompt the user for how many times they'd like that word repeated. Use the skills you learned in this module to print the word the correct number of times.
+random_word = input("Please pick a random word ")
 
+num_times = input ("How many times shall I repeat your word?")
+
+answer = (random_word) * (int(num_times))
+
+print (answer)
