b3b2b73ee59eaced2729f380d22f24f08f430d5c
Kerry G <kerrylyn@gmail.com>
2025-04-30 00:25:41 -0700

    Update practiceproblem3.py

--- Mod 3/practiceproblem3.py
+++ Mod 3/practiceproblem3.py
@@ -6,3 +6,13 @@ print out whether the word was found in the sentence. (i.e. True or False)
 sentence = input ("Please write a sentence.")
 
 print ("cloud" in sentence)
+'''
+Updated solve for this practive problem.
+'''
+
+sentence = input ("Please write a sentence.")
+word = input ("Please enter a word.")
+
+found = (word in sentence)
+
+print (found)
