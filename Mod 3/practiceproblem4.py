8627d4d433b99c74466eff9657fab6e47e076734
Kerry G <kerrylyn@gmail.com>
2025-04-30 00:36:12 -0700

    Update practiceproblem4.py

--- Mod 3/practiceproblem4.py
+++ Mod 3/practiceproblem4.py
@@ -8,3 +8,16 @@ str = (input ("Please type a word."))
 index1 = (input ("Please pick a first index "))
 index2 = (input ("Please pick a last index "))
 print (str [0:5])
+'''
+Updated solve for practice problem 4
+'''
+
+word = (input ("Please type a word."))
+
+begin_index = (int(input ("Enter a first index")))
+
+end_index = (int(input ("Enter a last index")))
+
+sliced_word = word [begin_index:end_index]
+
+print (sliced_word)
