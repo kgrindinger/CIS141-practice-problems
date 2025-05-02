4ab6da667f420e94578e04a95285665101032302
Kerry G <kerrylyn@gmail.com>
2025-05-02 15:53:12 -0700

    Update problem1.py

--- Mod 4/problem1.py
+++ Mod 4/problem1.py
@@ -1,1 +1,11 @@
+#1. Construct a truth table for the expression: (A AND B) OR (NOT B) where A and B each 
+# can be True or False. Your truth table should be commented out (as it's not valid Python code!)
 
+# List variables:
+# (A AND B), (NOT B)
+
+#A	B	   A AND B	  NOT B	  (A AND B)   OR (NOT B)
+#True	    True	    True	    False	      True
+#True	    False	    False	    True	      True
+#False	  True	    False	    False	      False
+#False	  False	    False	    True	      True
