5b5f0ac521bb9d25ec45b054ebc4b8bc3bf529e9
Kerry G <kerrylyn@gmail.com>
2025-05-05 17:14:44 -0700

    Update problem3.py

--- Mod 4/problem3.py
+++ Mod 4/problem3.py
@@ -1,8 +1,18 @@
 # Prompt the user for their bank balance. Evaluate whether the balance is less than $100. 
 # Print True if the user’s balance is below $100; print False, otherwise.
-
+'''
 balance =int(input ("What is your current bank balance?: "))
 if balance >= 100:
     print ("True - You have sufficient funds")
 if balance <= 100:
+    print ("False - Balance is too low")
+    '''
+
+# Prompt the user for their bank balance. Evaluate whether the balance is less than $100.
+# Print True if the user’s balance is below $100; print False, otherwise.
+
+balance=float(input ("What is your current bank balance?: "))
+if balance >= 100:
+    print ("True - You have sufficient funds")
+else:
     print ("False - Balance is too low")
