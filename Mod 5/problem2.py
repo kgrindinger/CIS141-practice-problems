553665e297d97c9d6c5ebb7de3875bd0b5d21d2f
Kerry G <kerrylyn@gmail.com>
2025-05-10 17:11:35 -0700

    Create problem2.py

---
+++ Mod 5/problem2.py
@@ -0,0 +1,7 @@
+#2. Define a string variable (e.g., my_string = "Olympic College"). Use a for loop to print
+# each character on its own line. Convert each character to uppercase before printing.
+
+string = "Rock and Roll"
+for char in string:
+    print(char)
+print("Exited for loop")
