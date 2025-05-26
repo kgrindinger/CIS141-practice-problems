334375a823b524b8ca2d5d54cdc7f61a9772ac79
Kerry G <kerrylyn@gmail.com>
2025-05-25 18:42:12 -0700

    problem1.py

--- Mod 7/problem1.py
+++ Mod 7/problem1.py
@@ -1,7 +1,7 @@
 #1. Write a function called count_vowels(input) that takes a string
 # and returns the number of vowels (a, e, i, o, u) in it.
 
-#I will use some book titles that I can see rom my current position as the testers
+#I will use some book titles that I can see from my current position as the testers
 #input strings
 #output boolean
 # I want to see the number of vowels per word, per title
